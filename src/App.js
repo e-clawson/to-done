8ad9d35fa067772bc269d436889c594a3e5a9e8a
Elizabeth Clawson <emclawson1@gmail.com>
2023-07-18 19:11:25 -0500

    add data and useState to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from "./data.json"
 import './App.css';
 import { Header } from './Header';
 
+
 function App() {
+  const  [ toDoList,setToDoList ] = useState(data);
+  
   return (
     <div className="App">
       <Header pageName= "To-Done!" slogan= "Track your progress!"/>
