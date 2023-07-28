e79e545c571c53ef43545606eef5c6e186dcbfec
Elizabeth Clawson <emclawson1@gmail.com>
2023-07-28 12:22:29 -0500

    update ToDoForm

--- src/ToDoForm.js
+++ src/ToDoForm.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
 
-const ToDoForm = (addTask) => { 
+const ToDoForm = ({ addTask }) => { 
     const [ userInput, setUserInput ] = useState('');
-
-    <input value={userInput} type="text" onChange={handleChange} placeholder="Enter Task"/>
 
     const handleChange = (e) => {
         setUserInput(e.target.value)
-
     }
 
     const handleSubmit = (e) => { 
-        e.preventDefault;
+        e.preventDefault();
         addTask(userInput);
         setUserInput("");
     }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input value={userInput} type="text" onChange={handleChange} placeholder="Enter Task"/>
+            <button>Add to List</button>
+        </form>
+    )
 
 } 
 export default ToDoForm;
